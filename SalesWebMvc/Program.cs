20362526fa1a6266b7407e09b2183f22e70ccd46
Alexandre Gava <alexandre.log@hotmail.com>
2022-12-28 19:40:39 -0300

    Seedingservice

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using static System.Formats.Asn1.AsnWriter;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMvcContext"))));
-
-//Another version from Anelio
+//Another version from Anelio:
 //options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"), builder => builder.MigrationsAssembly("SalesWebMvc")));
+
+builder.Services.AddTransient<SeedingService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -20,6 +25,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
