988e55e18bf55cf25b1c57657e36877a7e73658e
Alexandre Gava <alexandre.log@hotmail.com>
2022-12-29 08:49:51 -0300

    test new branch

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -42,10 +42,15 @@ namespace SalesWebMvc.Models
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
+
             Name = name;
+
             Email = email;
+
             BirthDate = birthDate;
+
             BaseSalary = baseSalary;
+
             Department = department;
         }
 
