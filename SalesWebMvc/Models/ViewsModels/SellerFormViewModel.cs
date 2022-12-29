f9074d75765d7ee9ea76c2ab6631c4ed95057965
Alexandre Gava <alexandre.log@hotmail.com>
2022-12-28 21:42:51 -0300

    delete seller

---
+++ SalesWebMvc/Models/ViewsModels/SellerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+
+namespace SalesWebMvc.Models.ViewsModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
